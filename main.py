78d93b710a00848f0083914083f0ffea510607e8
brun-brito <brunocaudebrito@hotmail.com>
2024-09-11 18:05:05 -0300

    Correção do uso da variável PORT

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
+import os
 from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
-# Rota para servir o HTML
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para servir o arquivo XML gerado
 @app.route('/xml')
-def xml_file():
+def get_xml():
     return send_file('dados_convertidos.xml', mimetype='application/xml')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
